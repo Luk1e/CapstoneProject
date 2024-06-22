43af7ce035bb5a11dcf751c596b84f53789f2fd5
Luk1e <luka.gogiashvili.02@gmail.com>
2024-06-22 17:04:50 +0400

    Feat: Added gh-pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CapstoneProject/",
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://capstoneproject-7f9w.onrender.com",
         changeOrigin: true,
         secure: false,
       },
